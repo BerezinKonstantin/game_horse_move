ae33fbb6f4514cb23a8633c9fd37daa6e2375fd0
Berezin Konstantin <berezinko90@gmail.com>
2023-02-17 16:28:39 +0700

    fix

--- src/models/Board.ts
+++ src/models/Board.ts
@@ -18,25 +18,24 @@ export class Board {
   }
 
   public highlightCells(selectedCell: Cell | null) {
-    let currentAviableCells: Cell[] = [];
+    let newAviableCells: Cell[] = [];
     for (let i = 0; i < this.cells.length; i++) {
       const row = this.cells[i];
       for (let j = 0; j < row.length; j++) {
         const target = row[j];
         if ((target.available = !!selectedCell?.canBeNext(target))) {
-          currentAviableCells.push(target);
+          newAviableCells.push(target);
         }
       }
     }
-    return (this.newAviableCells = currentAviableCells);
+    return (this.aviableCells = newAviableCells);
   }
 
   public getCopyBoard(): Board {
     const newBoard = new Board();
     newBoard.cells = this.cells;
-    newBoard.aviableCells = this.newAviableCells;
-    newBoard.newAviableCells = this.newAviableCells;
-    if (newBoard.newAviableCells.length === 0) {
+    newBoard.aviableCells = this.aviableCells;
+    if (newBoard.aviableCells.length === 0) {
       newBoard.isLose = true;
     }
     return newBoard;
